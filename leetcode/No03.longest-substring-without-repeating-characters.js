1f42a4851a019c7098f783e04b2a2724fb0bbe90
ecmadao <wlec@outlook.com>
2017-09-20 18:55:22 +0800

    WHAT: Daily leetcode
    WHY:
    
    * XXXXX
    
    HOW:
    
    * NOTHING TO SAY

--- leetcode/No03.longest-substring-without-repeating-characters.js
+++ leetcode/No03.longest-substring-without-repeating-characters.js
@@ -43,3 +43,34 @@ var lengthOfLongestSubstring = function(s) {
 };
 
 lengthOfLongestSubstring('abcabcbb'); // abc, 3
+
+/* ================= 优化版本 ================= */
+
+/**
+ * @param {string} s
+ * @return {number}
+ * 遍历数组，并已值为键，记录其索引和子字符串起始位置 start
+ * 遇见重复的元素后（当前索引为 i，重复元素的索引为 index），比较 index - start, i - start, i - index
+ * 取最长，并更新 start
+ */
+var lengthOfLongestSubstring = function(s) {
+  var temp = {};
+  var tempLength = 0;
+  var max = 0;
+  var start = 0;
+
+  for (var i = 0; i < s.length; i += 1) {
+    var str = s[i];
+    if (temp[str] === undefined || temp[str] < start) {
+      tempLength += 1;
+    } else {
+      var index = temp[str];
+      max = Math.max(max, index - start, i - index, i - start);
+      start = index + 1;
+      tempLength = i - index;
+    }
+    temp[str] = i;
+  }
+  max = Math.max(max, tempLength);
+  return max;
+};
