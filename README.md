9d80958efeefdc6751a47daf79db976cb6b0b406
ecmadao <wlec@outlook.com>
2017-11-13 21:57:02 +0800

    WHAT: Daily leetcode
    WHY:
    
    * XXXXX
    
    HOW:
    
    * NOTHING TO SAY

--- README.md
+++ README.md
@@ -122,6 +122,7 @@ Algorithms notes & code & leetcode solution by Node.js.
 - No.92 `Medium` [Reverse Linked List II](./leetcode/No92.reverse-linked-list-II.js)
 - No.93 `Medium` [Restore IP Addresses](./leetcode/No93.restore-ip-addresses.js)
 - No.94 `Medium` [Binary Tree Inorder Traversal](./leetcode/No94.binary-tree-inorder-traversal.js)
+- No.96 `Medium` [Unique Binary Search Trees](./leetcode/No96.unique-binary-search-trees.js)
 - No.118 `Easy` [Pascal's Triangle](./leetcode/No118.pascal's-triangle.js)
 - No.119 `Easy` [Pascal's Triangle II](./leetcode/No119.pascal's-triangle-II.js)
 - No.136 `Easy` [Single Number](./leetcode/No136.single-number.js)
---
+++ leetcode/No96.unique-binary-search-trees.js
@@ -0,0 +1,51 @@
+/**
+ * Difficulty:
+ * Medium
+ *
+ * Desc:
+ * Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+ *
+ * Example:
+ * Given n = 3, there are a total of 5 unique BST's.
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+ *
+ * 已知二叉树的取值范围是 1 ~ n，每个数字只出现一次。求总共有多少种不同的二叉树
+ */
+
+/**
+ * 思路：
+ * 动态规划。对于每个树节点（由根节点，左子节点，右子节点组成），其能够排列的树的数目为左子节点的数目 * 右子节点的数目
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function(n) {
+  const cache = {};
+  const array = [];
+  for (let i = 1; i <= n; i += 1) {
+    array.push(i);
+  }
+  const countTreeNum = (arr) => {
+    const len = arr.length;
+    if (!len || len === 1) return 1;
+    if (len === 2) return 2;
+    if (cache[len]) return cache[len];
+    let num = 0;
+    for (let i = 0; i < arr.length; i += 1) {
+      const leftCount = countTreeNum(arr.slice(0, i));
+      const rightCount = countTreeNum(arr.slice(i + 1));
+      num += (leftCount * rightCount);
+    }
+    cache[len] = num;
+    return num;
+  };
+
+  return countTreeNum(array);
+};
